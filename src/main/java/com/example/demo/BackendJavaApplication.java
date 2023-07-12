dff3ee0821e718534676719783f9a124b4ee74c3
Jirapong Kongsombutjaroen <nassurmi25@gmail.com>
2023-07-12 12:07:27 +0700

    add lab2

--- src/main/java/com/example/demo/BackendJavaApplication.java
+++ src/main/java/com/example/demo/BackendJavaApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendJavaApplication {
-
+	
 	public static void main(String[] args) {
+		System.out.println("Hello Spring!");
+		System.out.println("Spring boot");
 		SpringApplication.run(BackendJavaApplication.class, args);
 	}
 
